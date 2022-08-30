c985386cdf103cec9b6b7c1223e47312115ba15a
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2022-08-30 23:00:05 +0200

    Add files via upload

--- ML from scratch/logistic_regression/logistic_regression.py
+++ ML from scratch/logistic_regression/logistic_regression.py
@@ -6,7 +6,15 @@ class LogisticRegression:
         self.learning_rate = learning_rate
     
     def fit(self, X, y):
-        pass
+        self.w = np.zeros((X.shape[1], 1))
+        num_of_samples = X.shape[0]
+        
+        for epoch in range(self.num_epochs):
+            grad = np.zeros((X.shape[1], 1))
+            for i in range(num_of_samples):
+                x = X[i].reshape(-1, 1)
+                grad += (self.sigmoid(self.w.T@x)-y[i])*x
+            self.w -= self.learning_rate*grad
 
     def predict(self, X):
         pass
