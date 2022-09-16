367d2b21eb53706af2243c95ef674c21881b7c28
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2022-09-16 14:22:10 +0200

    Add files via upload

--- ML from scratch/logistic_regression/logistic_regression.py
+++ ML from scratch/logistic_regression/logistic_regression.py
@@ -1,23 +1,44 @@
+from re import L
 import numpy as np
+from tqdm import tqdm
 
 class LogisticRegression:
-    def __init__(self, num_epochs=500, learning_rate=0.01):
+    def __init__(self, num_epochs=500, learning_rate=0.01, threshold=0.5):
         self.num_epochs = num_epochs
         self.learning_rate = learning_rate
+        self.threshold = threshold
     
     def fit(self, X, y):
-        self.w = np.zeros((X.shape[1], 1))
+        X = self.prepare_features(X)
+        self.parameters = np.zeros((X.shape[1], 1))
         num_of_samples = X.shape[0]
         
-        for epoch in range(self.num_epochs):
+        for epoch in tqdm(range(self.num_epochs)):
             grad = np.zeros((X.shape[1], 1))
             for i in range(num_of_samples):
                 x = X[i].reshape(-1, 1)
-                grad += (self.sigmoid(self.w.T@x)-y[i])*x
-            self.w -= self.learning_rate*grad
+                grad += (self.sigmoid(self.parameters.T@x)-y[i])*x
+            self.parameters -= self.learning_rate*grad
 
     def predict(self, X):
-        pass
+        X = self.prepare_features(X)
+        y = np.zeros(X.shape[0])
+        self._check_fitted()
+        for i in range(len(X)):
+            x = X[i]
+            if self.sigmoid(self.parameters.T@x) >= self.threshold:
+                y[i] = 1
+            else:
+                y[i] = 0
+        return y
+
+    def _check_fitted(self):
+        if not hasattr(self, 'parameters'):
+            raise AttributeError("Model must be trained before making predictions.")
+
+    @staticmethod
+    def prepare_features(x):
+        return np.concatenate((x, np.ones((x.shape[0], 1))), axis=1)
 
     @staticmethod
     def sigmoid(x):
